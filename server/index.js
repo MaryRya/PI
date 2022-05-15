9eef79713489753b974718f452ece18f73a8d6d9
anyaphmv <stud0000231077@study.utmn.ru>
2022-05-15 16:10:55 +0500

    front

--- server/index.js
+++ server/index.js
@@ -14,14 +14,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(express.static(path.join(__dirname, 'public')))
 app.use(fileUpload({}))
 app.use('/api', router)
 app.use(errorHandler)
-
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`);
-});
 
 const start = async () => {
     try {
